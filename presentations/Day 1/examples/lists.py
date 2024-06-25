056063c3a3e8e21a4eeed7bc1cfb0f879bcb5fc3
spider-z3r0 <kevin.omalley@ul.ie>
2024-06-25 09:24:26 +0100

    making everything up to date

---
+++ presentations/Day 1/examples/lists.py
@@ -0,0 +1,11 @@
+# This is a list of integers
+
+l1 = [1, 2, 3, 4, 5]
+
+# This is a list of strings
+
+l2 = ['apple', 'banana', 'carrot', 'dumpling', 'egg']
+
+# This is a list of mixed types
+
+l3 = [1, 'apple', 'banana', 10.1, 'carrot', 'dumpling']
---
+++ presentations/Day 1/examples/strings.py
@@ -0,0 +1,16 @@
+# This is a string
+s1 = 'Hello, world!'
+
+# This is also a string
+
+s2 = "Hello, world!"
+
+# This is a multiline string
+
+s3 = '''Hello,
+world!'''
+
+# This is also a multiline string
+
+s4 = """Hello,
+world!"""
